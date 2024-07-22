f604aa17ef3a67382438e1f83b6a3a243ae4b3ce
Сергей Михалёв <snt_mail@bk.ru>
2024-07-22 22:40:43 +0500

    Экран выбора отрасли + выбор региона вместе со станой

--- app/src/main/java/ru/practicum/android/diploma/filter/ui/region/RegionViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/region/RegionViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.filter.domain.api.FilterInteractor
+import ru.practicum.android.diploma.filter.domain.models.Country
 import ru.practicum.android.diploma.filter.domain.models.Location
 import ru.practicum.android.diploma.filter.domain.models.Region
 import ru.practicum.android.diploma.search.domain.utils.ResponseData
@@ -18,6 +19,7 @@ class RegionViewModel(
     private val _screenState = MutableLiveData<RegionState>(RegionState.Loading)
     val screenState: LiveData<RegionState> = _screenState
 
+    private val countriesList: MutableList<Country> = ArrayList()
     private val originalList: MutableList<Region> = ArrayList()
     private val filteredList: MutableList<Region> = ArrayList()
 
@@ -26,10 +28,26 @@ class RegionViewModel(
     var region: Region?
         get() = location.region
         set(value) {
-            location = location.copy(region = value)
+            location = location.copy(
+                country = findCountry(value?.parentId),
+                region = value
+            )
         }
 
     init {
+        viewModelScope.launch(Dispatchers.IO) {
+            filterInteractor.getCountries().collect { data ->
+                when (data) {
+                    is ResponseData.Data -> {
+                        countriesList.addAll(data.value)
+                    }
+
+                    is ResponseData.Error -> {
+                        println("Error on getting countries list")
+                    }
+                }
+            }
+        }
         viewModelScope.launch(Dispatchers.IO) {
             val country = location.country
             if (country != null) {
@@ -58,7 +76,6 @@ class RegionViewModel(
     }
 
     fun search(searchText: String?) {
-        println("searcging $searchText")
         filteredList.clear()
         if (searchText.isNullOrEmpty()) {
             _screenState.postValue(RegionState.Content(originalList))
@@ -71,4 +88,12 @@ class RegionViewModel(
             _screenState.postValue(RegionState.Content(filteredList))
         }
     }
+
+    private fun findCountry(id: Int?): Country {
+        id.let {
+            val result = countriesList.filter { it.id == id }.first()
+            return result
+        }
+    }
+
 }
